17847030b1a72568a9220f5c2fb9ccf31c18a7a0
Uriel Miranda <miranda.uriel@gmail.com>
2020-04-08 09:35:15 -0300

    Add i18n translations to project

---
+++ pharmacy-client/src/translations/en.js
@@ -0,0 +1,6 @@
+export default {
+  documentId: "Document ID",
+  companyId: "Company ID",
+  documentIdRequired: "Document ID is required",
+  companyIdRequired: "Company ID is required"
+};
---
+++ pharmacy-client/src/translations/pt.js
@@ -0,0 +1,6 @@
+export default {
+  documentId: "CPF",
+  companyId: "CNPJ",
+  documentIdRequired: "Campo de CPF é obrigatório",
+  companyIdRequired: "Campo de CNPJ é obrigatório",
+};
