b957eea9565109f8a67e8120ca0d1e22da702167
Uriel Miranda <miranda.uriel@gmail.com>
2020-04-07 21:01:06 -0300

    Store prescriptions

--- api-server/src/routes/prescriptions.js
+++ api-server/src/routes/prescriptions.js
@@ -5,10 +5,21 @@ import { PrescriptionRepository, DoctorRepository } from "../repositories";
 const Router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
-Router.post("/", upload.single("prescriptionFile"), async (req, res) => {
+Router.post("/", async (req, res) => {
   try {
-    const doctor = await DoctorRepository.store.getOrCreate(req.body.doctor);
-    await PrescriptionRepository.store.create(doctor, req.body);
+    const ip =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+    const document = req.body.prescriptionFile;
+    const reqDoctor = JSON.parse(req.body.doctor);
+    const data = req.body;
+    reqDoctor.documentId = data.doctorId;
+    const doctor = await DoctorRepository.store.getOrCreate(reqDoctor);
+    data.digitalSignature = reqDoctor.block;
+    data.ip = ip;
+    await PrescriptionRepository.store.create(doctor, document, data);
     return res.success();
   } catch (err) {
     return res.error(err);
