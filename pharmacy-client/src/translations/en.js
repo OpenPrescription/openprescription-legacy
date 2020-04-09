e59a0628fe75e13c4fb79660dd3def91bbaf0033
Uriel Miranda <miranda.uriel@gmail.com>
2020-04-08 21:46:22 -0300

    Prescription server request

--- pharmacy-client/src/translations/en.js
+++ pharmacy-client/src/translations/en.js
@@ -2,5 +2,7 @@ export default {
   documentId: "Document ID",
   companyId: "Company ID",
   documentIdRequired: "Document ID is required",
-  companyIdRequired: "Company ID is required"
+  companyIdRequired: "Company ID is required",
+  prescriptionNotFoundError: "Prescription not found",
+  prescriptionUnexpectedError: "Unexpected error. Try again later."
 };
--- pharmacy-client/src/translations/pt.js
+++ pharmacy-client/src/translations/pt.js
@@ -3,4 +3,6 @@ export default {
   companyId: "CNPJ",
   documentIdRequired: "Campo de CPF é obrigatório",
   companyIdRequired: "Campo de CNPJ é obrigatório",
+  prescriptionNotFoundError: "Receita médica não pode ser encontrada em nosso sistema.",
+  prescriptionUnexpectedError: "Ocorreu um erro insperado. Tente fazer a consulta mais tarde."
 };
