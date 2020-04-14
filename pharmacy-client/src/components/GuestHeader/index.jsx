e7572c640c1a056b0f0ab272a8faada348f773b5
Edilson Osorio Jr <osoriojr@gmail.com>
2020-04-14 14:22:30 +0300

    Hotfix lang (#7)
    * fix pharmacy lang
    
    * Added language selector GuestHeader
    updates .gitignore
    
    * updated .gitignore
    
    * remove comments

--- pharmacy-client/src/components/GuestHeader/index.jsx
+++ pharmacy-client/src/components/GuestHeader/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -7,27 +7,81 @@ import IconButton from "@material-ui/core/IconButton";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { useTranslation } from "react-i18next";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/Button";
+
+
 
 import logo from "../../assets/logo.svg";
 
 const useStyles = makeStyles((theme) => ({
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   icon: {
     marginRight: theme.spacing(2),
   },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  title: {
+    flexGrow: 1,
+    textTransform: 'uppercase'
+  },
+  menuItemBlock: {
+    display: 'block',
+    width: '100%'
+  }
 }));
+
 
 export default () => {
   const classes = useStyles();
+  const { i18n } = useTranslation();
+  const [language, setLanguage] = useState(i18n.language);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleSelectLanguage = (event, lang) => {
+    if (!lang) return setAnchorEl(event.currentTarget);
+    i18n.changeLanguage(lang);
+    setLanguage(lang);
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <header>
       <CssBaseline />
       <AppBar position="relative">
-        <Toolbar>
+        <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
           <img src={logo} />
+          <Button
+            color="inherit"
+            aria-controls="language-menu"
+            aria-haspopup="true"
+            onClick={handleSelectLanguage}
+          >
+            {language}
+            <KeyboardArrowDownIcon />
+          </Button>
         </Toolbar>
+        <Menu
+          id="language-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          keepMounted
+        >
+          <MenuItem className={classes.menuItemBlock} onClick={(e) => handleSelectLanguage(e, "en")}>
+            English
+          </MenuItem>
+          <MenuItem className={classes.menuItemBlock} onClick={(e) => handleSelectLanguage(e, "pt")}>
+            Português
+          </MenuItem>
+        </Menu>
       </AppBar>
     </header>
   );
